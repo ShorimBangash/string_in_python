8af4477d9b5c88d3a73fec0a688d6616be2e09df
ShorimBangash <86353138+ShorimBangash@users.noreply.github.com>
2021-06-24 15:50:24 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+message="                We are the students of Bscs "
+
+print(message.strip())
+#strip are used to colips all space in sentance (from starting and Ending)...
+
+
+print(message.replace("Bscs" , "Bsse"))
+#reolace are used to change an word in sentance...
+
+print(message.split())
+#split are used to split the sentance...
+
